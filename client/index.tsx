719705b14fc8fb30109abfde3e50cfd1b2d3161b
Kafva <jonas@kafva.one>
2022-07-31 09:18:56 +0200

    Basic worker

--- client/index.tsx
+++ client/index.tsx
@@ -4,6 +4,23 @@ import './scss/index.scss';
 import App from './components/App';
 import { SetupMediaHandlers, HandleKeyboardEvent } from './controls';
 
+// The `import.meta.url` argument is needed to create the name 
+// of the compiled version of `worker.js`
+const worker = new Worker(new URL('./worker.ts', import.meta.url))
+
+worker.onmessage = function(e) {
+  console.log('Message received from worker', e.data);
+}
+
+setTimeout( () => {
+  (()=>{
+    console.log('Message posted to worker');
+    worker.postMessage([5, 5]);
+  })()
+}, 2000)
+
+
+
 // Setup global listeners for keyboard and media key events
 window.addEventListener("keydown", HandleKeyboardEvent);
 SetupMediaHandlers()
