8379818d1317df3042853d55b3277060641c9c5f
Kafva <jonas@kafva.one>
2022-09-16 21:42:18 +0200

    Capture frequency data

--- client/components/Player.tsx
+++ client/components/Player.tsx
@@ -74,6 +74,8 @@ const Player = (props: {
   isPlaying: boolean
 }) => {
   let audio: HTMLAudioElement;
+  let analyser: AnalyserNode;
+  let frequencyData: Uint8Array;
 
   const [volume,setVolume] = createSignal(Config.defaultVolume)
   const [currentTime,setCurrentTime] = createSignal(0)
@@ -161,6 +163,34 @@ const Player = (props: {
     // Initialise the <audio> with the desired default volume
     audio     = GetHTMLElement<HTMLAudioElement>("audio")
     audio.volume = volume()
+
+    // https://developer.mozilla.org/en-US/docs/Web/API/AudioNode
+    // Source nodes:      0  inputs, 1+ outputs
+    // Destination nodes: 1+ inputs, 0  outputs
+    // Analyser nodes:    1+ inputs, 1+ outputs
+    //
+    // Connect the OUTPUT of `node1` to the INPUT of `node2`
+    // node1.connect(node2)
+
+    // Initialise audio context for sound visualisation
+    const audioCtx = new window.AudioContext();
+    
+    // Create a SRC node (1 output no input) for the <audio>
+    const srcNode = audioCtx.createMediaElementSource(audio)
+
+    // Create an analyser which can read the frequency data of the node
+    //  maxDecibels: -30
+    //  minDecibels: -100
+    analyser = audioCtx.createAnalyser();
+
+    frequencyData = new Uint8Array(analyser.frequencyBinCount)
+
+    // Set the OUTPUT of the `srcNode` to the INPUT of the analyser...
+    srcNode.connect(analyser)
+    // ...and set the OUTPUT of the analyzer to be the INPUT for 
+    // the audio context destination
+    analyser.connect(audioCtx.destination)
+
   })
 
   return (<>
@@ -172,6 +202,12 @@ const Player = (props: {
         if (audio.currentTime <= props.track.Duration) {
           setCurrentTime(audio.currentTime)
         }
+        if (analyser) {
+          // Note: the array will be zeroed out if the audio is muted.
+          analyser.getByteFrequencyData(frequencyData)
+          Log("Frequency data", frequencyData)
+        }
+
       }}
       onLoadedData={ () => {
         // Resume playback if needed
--- client/index.tsx
+++ client/index.tsx
@@ -9,49 +9,13 @@ SetupMediaHandlers()
 
 render(() => <App/>, document.getElementById('root') as HTMLElement);
 
-let analyser: AnalyserNode;
-let frequencyData: Uint8Array;
-
-// https://developer.mozilla.org/en-US/docs/Web/API/AudioNode
-// Source nodes:      0  inputs, 1+ outputs
-// Destination nodes: 1+ inputs, 0  outputs
-// Analyser nodes:    1+ inputs, 1+ outputs
-//
-// Connect the OUTPUT of `node1` to the INPUT of `node2`
-// node1.connect(node2)
-let srcNode: AudioNode;
-
-window.onload = () => {
-  setTimeout(()=>{
-    // Initialise audio context for sound visualisation
-    const audioCtx = new window.AudioContext();
-    const audio = document.querySelector('audio');
-    
-    // Create a SRC node (1 output no input) for the <audio>
-    srcNode = audioCtx.createMediaElementSource(audio!)
-
-    // Create an analyser which can read the frequency data of the node
-    //  maxDecibels: -30
-    //  minDecibels: -100
-    analyser = audioCtx.createAnalyser();
-
-    frequencyData = new Uint8Array(analyser.frequencyBinCount)
-
-    // Set the OUTPUT of the `srcNode` to the INPUT of the analyser...
-    srcNode.connect(analyser)
-    // ...and set the OUTPUT of the analyzer to be the INPUT for 
-    // the audio context destination
-    analyser.connect(audioCtx.destination)
 
 
-    console.log("Created context for", audio!.src)
-  }, 4000)
-}
+//window.onload = () => {
+//  setTimeout(()=>{
+//    console.log("Created context for", audio!.src)
+//  }, 4000)
+//}
 
-setInterval( () => {
-  if (analyser) {
-    // Note: the array will be zeroed out if the audio is muted.
-    analyser.getByteFrequencyData(frequencyData)
-    console.log("AAAAAAAA", analyser, frequencyData)
-  }
-}, 4000)
+//setInterval( () => {
+//}, 4000)
