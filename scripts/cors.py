b938e3e1264e568c508bc960fa8451c45d45edb1
Kafva <jonas@kafva.one>
2023-02-06 21:23:54 +0100

    autofmt: black

--- scripts/cors.py
+++ scripts/cors.py
@@ -5,12 +5,15 @@ served with valid CORS settings.
 """
 import os
 
+
 class AddHeader:
     def response(self, flow):
-        # One can techincally limit this to 
+        # One can techincally limit this to
         #   'https://lofy:20111 https://youtube.com'
         # but other sites that use YouTube resources could still break
-        flow.response.headers["access-control-allow-origin"] = \
-                os.environ["CORS"] if "CORS" in os.environ else "*"
+        flow.response.headers["access-control-allow-origin"] = (
+            os.environ["CORS"] if "CORS" in os.environ else "*"
+        )
+
 
 addons = [AddHeader()]
